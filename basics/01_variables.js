1f74b3473fac92eafa7208c7acc2f088a33838a2
rajnidogra <rajni.pdogra@gmail.com>
2026-06-25 09:05:13 +0000

    variables done

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "rajni@gmail.com"
+var accountPassword = 123456
+acoountCity = "Delhi"
+let accountState;
+
+//accountId = 2
+
+accountEmail = "raj19@gmail.com"
+accountPassword = 612354
+acoountCity = "Kangra"
+
+//console.log(accountId)
+
+console.table([accountEmail,accountPassword,acoountCity,accountState])
